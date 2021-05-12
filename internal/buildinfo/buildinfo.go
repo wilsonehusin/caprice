dfb8f543b2425aaeb5e644e0beea6abc982c0a7f
Wilson E. Husin <wilson@husin.dev>
2021-05-11 22:36:57 -0700

    Nullify Go version

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -3,7 +3,7 @@ package buildinfo
 var Version = "v0.1.0"
 var GitSHA = "000000"
 var Server = "http://mini.caprice.run"
-var Go = "420.69"
+var Go = "0.0.0.0.0.0.0"
 
 func All() *map[string]string {
 	return &map[string]string{
